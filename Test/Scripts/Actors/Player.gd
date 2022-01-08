276433041321704989f1765f225f83918f9a2ccd
MignoneNguyen <ducksfromandromeda@gmail.com>
2022-01-08 22:13:28 +0700

    Movement & Collision Update
    My man can move around now yeet

--- Test/Scripts/Actors/Player.gd
+++ Test/Scripts/Actors/Player.gd
@@ -2,8 +2,8 @@ extends Actor
 
 func _physics_process(delta: float) -> void:
 	var direction: = Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("up") - Input.get_action_strength("down")
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		0.0
 		).normalized()
 	
 	if not is_on_floor():
