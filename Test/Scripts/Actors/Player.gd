edbe60eb117d2dbcfab1a129d95eb366d59e6ba4
Crazycamp <94753843+Crazycamp@users.noreply.github.com>
2022-01-21 16:02:56 +0700

    Added assets

--- Test/Scripts/Actors/Player.gd
+++ Test/Scripts/Actors/Player.gd
@@ -6,15 +6,16 @@ func _physics_process(delta: float):
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		- 1.0 if Input.is_action_pressed("jump") and is_on_floor()  else 0.0 and is_on_ceiling()
 		).normalized()
-	
+
 	if not is_on_floor():
 		_velocity_y -= gravity * delta
-	
+
 	var _velocity = Vector3(
 		direction.x * speed,
 		_velocity_y,
 		0.0)
 	move_and_slide(_velocity, FLOOR_NORMAL)
+	
 	
 	if is_on_floor() or is_on_ceiling():
 		_velocity_y = 0.0
