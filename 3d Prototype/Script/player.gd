a614009c56df2c781a92ba080c2992841e723d70
Crazycamp <94753843+Crazycamp@users.noreply.github.com>
2022-02-26 13:52:46 +0700

    Running & Camera

--- 3d Prototype/Script/player.gd
+++ 3d Prototype/Script/player.gd
@@ -8,7 +8,7 @@ func _physics_process(delta) -> void:
 	var direction_ground: = Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		Input.get_action_strength("move_backwards") - Input.get_action_strength("move_towards")).normalized()
-		
+		Input.get_action_strength("running")
 	if not is_on_floor():
 		velocity_y -= gravity * delta
 	
