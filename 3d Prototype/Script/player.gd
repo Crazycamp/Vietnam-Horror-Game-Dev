ae0c7cefd78a515fb19c265189bfbcc44bb5b1c8
MignoneNguyen <ducksfromandromeda@gmail.com>
2022-03-12 01:54:17 +0700

    Need the Animation Sprite
    The code was put there and will be changed once there is an Animation Sprite

--- 3d Prototype/Script/player.gd
+++ 3d Prototype/Script/player.gd
@@ -2,6 +2,8 @@ extends Actor
 
 onready var sprint_timer = $SprintTimer
 var sprinting = false
+var a = 0
+var current_animation = "idle"
 
 func playerSetup (pos : Vector3):
 	global_transform.origin = pos
@@ -12,6 +14,20 @@ func _physics_process(delta) -> void:
 	var direction_ground: = Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		Input.get_action_strength("move_backwards") - Input.get_action_strength("move_towards")).normalized()
+		
+	if Input.is_action_pressed("move_right"):
+		current_animation = "idle"
+	if Input.is_action_pressed("move_left"):
+		current_animation = "idle"
+	if Input.is_action_pressed("move_backwards"):
+		current_animation = "idle"
+	if Input.is_action_pressed("move_towards"):
+		current_animation = "idle"
+	
+	if direction_ground.length() != 0:
+		a = direction_ground.angle() / (PI/4)
+		a = wrapi(int(a), 0, 8)
+		current_animation = "run"
 	
 	if Input.is_action_pressed("sprint"):
 		sprint_timer.start()
@@ -32,6 +48,10 @@ func _physics_process(delta) -> void:
 	)
 	move_and_slide(velocity, FLOOR_NORMAL)
 	
-	
+	# If the code cannot run, please delete the line below then insert it back
+	# Currently we still don't have AnimatedSprite yet so the line below cannot executed
+	$AnimatedSprite.animation = current_animation + str(a)
+
+		
 func _on_SprintTimer_timeout() -> void:
 	sprinting = false
