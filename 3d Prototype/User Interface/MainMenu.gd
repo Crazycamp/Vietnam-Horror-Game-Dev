91b5d04f3036e1908066c8e736e1c50862170be3
Crazycamp <94753843+Crazycamp@users.noreply.github.com>
2022-03-27 11:17:21 +0700

    Stuff

--- 3d Prototype/User Interface/MainMenu.gd
+++ 3d Prototype/User Interface/MainMenu.gd
@@ -4,4 +4,4 @@ extends Button
 func _ready():
 	pass # Replace with function body.
 func _on_Start_pressed():
-	get_tree().change_scene("res://New Prototype/Levels/School.tscn")
+	get_tree().change_scene("res://Main Game/Levels/schoolvers2.tscn")
